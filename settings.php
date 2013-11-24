2da0ba7270981490941c4c92c9ab64c896435640
Kevin Zittle <kevin@f4design.com>
2013-11-24 12:38:14 -0800

    Hue Settings
    Got rid of initialize because can't pragmatically create user. Adjusted settings to walk users through the process.

--- settings.php
+++ settings.php
@@ -4,7 +4,16 @@
 
 //API Keys
 $hue_bridge = '10.0.1.13'; //IP Address of Hue => http://www.meethue.com/api/nupnp
-$hue_user = 'dashboard'; //Username that will be created on your Hue device. Default is dashboard, but choose whatever you want.
+
+/* You have to create a new user first. Go to http://HUEIPHERE/debug/clip.html then enter the following information:
+	URL: http://HUEIPHERE/api
+	MESSAGE BODY: {"devicetype":"user","username":"newdeveloper"}
+	Click POST
+	Press the button on your Hue Bridge
+	Click POST again. You should get a success message in Command Response
+*/
+
+$hue_user = 'newdeveloper'; //Username that you created on your Hue device. 
 $hue_url = 'http://' . $hue_bridge . '/api'; //url of Hue bridge
 $hue_api_url = $hue_url . '/' . $hue_user . '/';
 
