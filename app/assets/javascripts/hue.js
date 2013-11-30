d74b38d963c03b773c4bd0d656a3231e7d490fd6
Kevin Zittle <kevin@f4design.com>
2013-11-30 15:45:30 -0800

    Hue
    Added some new icons and new scenes.

--- app/assets/javascripts/hue.js
+++ app/assets/javascripts/hue.js
@@ -14,6 +14,11 @@ function hue1(n,w) {
 	  break; 
 	case "hue1_white":
 	  xmlhttp.send('{"ct":0}');
+	case "hue1_fall":
+	  xmlhttp.send('{"on":true, "bri":124, "hue":6491, "sat":213, "ct":500}');
+	  break;
+	case "hue1_night":
+	  xmlhttp.send('{"on":true, "bri":1, "hue":46988, "sat":254, "xy":[0.1683, 0.0426], "ct":500}');
 	  break;
 	default:
 
@@ -37,6 +42,15 @@ function hue2(n,w) {
 	case "hue2_white":
 	  xmlhttp.send('{"ct":0}');
 	  break;
+	case "hue2_fall_wakeup":
+	  xmlhttp.send('{"on":true, "bri":2, "hue":2005, "sat":206, "ct":500}');
+	  break;
+	case "hue2_fall":
+	  xmlhttp.send('{"on":true, "bri":124, "hue":2005, "sat":206, "ct":500}');
+	  break;
+	case "hue2_night":
+	  xmlhttp.send('{"on":true, "bri":7, "hue":49344, "sat":254, "xy":[0.1943, 0.0551], "ct":500}');
+	  break;
 	default:
 
 	}
@@ -59,6 +73,12 @@ function hue3(n,w) {
 	case "hue3_white":
 	  xmlhttp.send('{"ct":0}');
 	  break;
+	case "hue3_fall":
+	  xmlhttp.send('{"on":true, "bri":124, "hue":11228, "sat":165, "ct":432}');
+	  break;
+	case "hue3_night":
+	  xmlhttp.send('{"on":true, "bri":3, "hue":46988, "sat":254, "xy":[0.1683, 0.0426], "ct":500}');
+	  break;
 	default:
 
 	}
@@ -66,6 +86,7 @@ function hue3(n,w) {
 //
 
 $(document).ready(function(){
+	//Hue 1
 	$("#hue1_on").click(function(){
 	  hue1("hue1_on");
 	});
@@ -75,6 +96,8 @@ $(document).ready(function(){
 	$("#hue1_white").click(function(){
 	  hue1("hue1_white");
 	});
+	
+	//Hue 2
 	$("#hue2_on").click(function(){
 	  hue2("hue2_on");
 	});
@@ -84,6 +107,11 @@ $(document).ready(function(){
 	$("#hue2_white").click(function(){
 	  hue2("hue2_white");
 	});
+	$("#hue2_wakeup").click(function(){
+	  hue2("hue2_fall_wakeup");
+	});
+	
+	//Hue 3
 	$("#hue3_on").click(function(){
 	  hue3("hue3_on");
 	});
@@ -93,10 +121,22 @@ $(document).ready(function(){
 	$("#hue3_white").click(function(){
 	  hue3("hue3_white");
 	});
+	
+	//All Hues
 	$("#allhue_off").click(function(){
 	  hue1("hue1_off");
 	  hue2("hue2_off");
 	  hue3("hue3_off");
+	});
+	$("#allhue_fall").click(function(){
+	  hue1("hue1_fall");
+	  hue2("hue2_fall");
+	  hue3("hue3_fall");
+	});
+	$("#allhue_night").click(function(){
+	  hue1("hue1_night");
+	  hue2("hue2_night");
+	  hue3("hue3_night");
 	});
 });
 function returnpercentage(w) {
