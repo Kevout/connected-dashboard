9efe71faaa0b73e4ebef8b3ca3d2ce430712c384
Kevin Zittle <kevin@f4design.com>
2013-11-29 22:13:35 -0800

    Got Hue working with a basic connection.

---
+++ app/assets/javascripts/hue3.js
@@ -0,0 +1,39 @@
+function lampstuur(n,w) {
+	xmlhttp = new XMLHttpRequest();
+	xmlhttp.open('PUT',
+'http://10.0.1.13/api/newdeveloper/lights/3/state',true);
+
+
+	switch(n)
+	{
+	case "lamp_on":
+	  xmlhttp.send('{"on":true}');
+	  break;
+	case "lamp_off":
+	  xmlhttp.send('{"on":false}');
+	  break; 
+	case "lamp_white":
+	  xmlhttp.send('{"ct":0}');
+	  break;
+	default:
+
+	}
+}
+//
+
+$(document).ready(function(){
+	$("#lamp_on").click(function(){
+	  lampstuur("lamp_on");
+	});
+	$("#lamp_off").click(function(){
+	  lampstuur("lamp_off");
+	});
+	$("#lamp_white").click(function(){
+	  lampstuur("lamp_white");
+	});
+});
+function returnpercentage(w) {
+w = w / 2.55;
+w = Math.floor(w) + "%"
+return w
+}
